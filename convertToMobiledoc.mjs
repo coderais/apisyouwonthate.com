5e59996f358b9308038389b50f9fe707530fdc1e
Jason <bijason@proton.me>
2023-03-24 08:05:10 +0600

    modify convert script to support auto import
    Changes:
    * Can auto-import to a Ghost blog using REST API
    * Fix migration errors by checking if posts are assigned to the right
      users and checking user roles

--- convertToMobiledoc.mjs
+++ convertToMobiledoc.mjs
@@ -1,10 +1,96 @@
+/**
+ * Convert from MDX to Ghost.
+ * Before doing anything, you need to:
+ * 1. Copy example.env file to .env.
+ * 2. Open .env file in your text editor and change variables
+ *
+ * @usage node ./convertToMobiledoc.mjs
+ * @usage To auto import to Ghost using REST API: node ./convertToMobiledoc.mjs --auto
+ * @file
+ */
 import fs from 'fs';
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import yaml from 'js-yaml';
 import JSZip from 'jszip';
+import axios from 'axios';
+import FormData from 'form-data';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
+
+export const GHOST_API = process.env.GHOST_API;
+const GHOST_SITE = process.env.GHOST_SITE;
+const GHOST_USER = process.env.GHOST_USER;
+const GHOST_PASSWORD = process.env.GHOST_PASSWORD;
+
+// All users except for these will be given "contributor" role
+export const ADMIN_USERS = process.env.ADMIN_USERS.split(',');
+
+let currentGhostCookie;
+
+/**
+ * Login to Ghost blog and save the cookie.
+ * @returns {Promise<String>} ghost admin api session key
+ */
+export async function createGhostUserSession() {
+  if (currentGhostCookie) {
+    return currentGhostCookie;
+  }
+
+  const resp = await axios.post(
+    `${GHOST_API}/session/`,
+    {
+      username: GHOST_USER,
+      password: GHOST_PASSWORD,
+    },
+    {
+      headers: {
+        Origin: GHOST_SITE,
+        'Accept-Version': 'v3.0',
+      },
+      withCredentials: true,
+    }
+  );
+
+  return resp.headers['set-cookie'][0]
+    .split(';')
+    .find((r) => r.includes('ghost-admin-api-session'));
+}
+
+/**
+ * @param {Object} options - axios options
+ * @returns {Object} axios result
+ */
+export async function requestGhostApi(options) {
+  const cookie = await createGhostUserSession();
+  const axiosOptions = Object.assign(
+    {
+      method: 'GET',
+      headers: {
+        'User-Agent': `Mozilla/5.0 (Windows NT 10.0; rv:110.0) Gecko/20100101 Firefox/110.0`,
+        Accept: 'application/json, text/javascript, */*; q=0.01',
+        'Accept-Language': 'en-US,en;q=0.5',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Content-Type': 'application/json; charset=UTF-8',
+        'X-Ghost-Version': '5.39',
+        'App-Pragma': 'no-cache',
+        'X-Requested-With': 'XMLHttpRequest',
+        DNT: '1',
+        Connection: 'keep-alive',
+        Referer: `${GHOST_SITE}/ghost/`,
+        Cookie: `${cookie}`,
+        'Sec-Fetch-Dest': 'empty',
+        'Sec-Fetch-Mode': 'cors',
+        'Sec-Fetch-Site': 'same-origin',
+      },
+    },
+    options
+  );
+  return await axios(axiosOptions);
+}
 
 const convertToMobiledoc = (markdown) => {
   const mobiledoc = JSON.stringify({
@@ -61,17 +147,32 @@ export function markdownToJson(markdown) {
  * @returns {Array} an array of user objects compatible with Ghost
  */
 export function exportUsers() {
-  const users = fs.readdirSync(`${__dirname}/src/content/authors`);
-  return users.map((userFile, index) => {
+  let userFiles = fs.readdirSync(`${__dirname}/src/content/authors`);
+
+  const users = userFiles.map((userFile, index) => {
     const markdown = fs.readFileSync(
       `${__dirname}/src/content/authors/${userFile}`,
       'utf8'
     );
     const slug = `${userFile.replace('.mdx', '')}`;
     const data = markdownToJson(markdown);
+
+    let id = index + 3;
+    switch (data.name) {
+      case ADMIN_USERS[0]:
+        id = 1;
+        break;
+      case ADMIN_USERS[1]:
+        id = 2;
+        break;
+
+      default:
+        break;
+    }
+
     return {
       slug,
-      id: index + 3,
+      id,
       name: data.name,
       email: data.email || `${slug}@example.com`,
       profile_image: `/content/images/${data.photo}`,
@@ -89,6 +190,74 @@ export function exportUsers() {
       updated_by: 1,
     };
   });
+
+  // Ignore admin users, don't export them
+  // return users.filter((u) => !ADMIN_USERS.includes(u.name));
+  return users;
+}
+
+/**
+ * Fetch all the user roles from a Ghost blog.
+ * @returns {Promise<Array<Object>>} array of roles
+ */
+export async function findUserRoles() {
+  const resp = await requestGhostApi({
+    method: 'GET',
+    url: `${GHOST_API}/roles/?permissions=assign`,
+  });
+
+  return resp.data.roles;
+}
+
+/**
+ * @param {String} name - the name of the role
+ * @returns {Promise<Object|undefined>}
+ */
+export async function findUserRoleByName(name) {
+  const roles = await findUserRoles();
+  return roles ? roles.find((r) => r.name === name) : undefined;
+}
+
+/**
+ * Find posts in a Ghost blog.
+ *
+ * @param {Number} limit
+ * @param {Number} page
+ * @returns {Promise<Object>}
+ */
+export async function findPosts(limit = 10, page = 1) {
+  const resp = await requestGhostApi({
+    url: `${GHOST_API}/posts/?formats=mobiledoc%2Clexical&limit=${limit}&page=${page}&filter=status%3Apublished`,
+  });
+  return resp.data;
+}
+
+/**
+ * Find users in a Ghost blog.
+ * @returns {Promise<Object>}
+ */
+export async function findUsers() {
+  const resp = await requestGhostApi({
+    url: `${GHOST_API}/users/?limit=all&include=roles`,
+  });
+  return resp.data;
+}
+
+/**
+ * Make all users except for admin users contributors.
+ *
+ * @param {Array} users Ghost users
+ * @returns {Promise<Array>} roles to users relation
+ */
+export async function exportRolesUsers(users) {
+  const contributorRole = await findUserRoleByName('Contributor');
+  const contributorRoleId = contributorRole.id;
+
+  return users
+    .filter((u) => !ADMIN_USERS.includes(u.name))
+    .map((user) => {
+      return { user_id: user.id, role_id: contributorRoleId };
+    });
 }
 
 /**
@@ -98,8 +267,8 @@ export function exportUsers() {
  * @returns {Array} an array of post objects compatible with Ghost.
  */
 export function exportPosts(users) {
-  const posts = fs.readdirSync(`${__dirname}/src/content/blog`);
-  return posts.map((postFile, index) => {
+  const postsFiles = fs.readdirSync(`${__dirname}/src/content/blog`);
+  return postsFiles.map((postFile, index) => {
     const markdown = fs.readFileSync(
       `${__dirname}/src/content/blog/${postFile}`,
       'utf8'
@@ -122,10 +291,22 @@ export function exportPosts(users) {
 
     const author = users.find((u) => u.name === meta.author);
     if (author) {
+      data.author_id = author.id;
       data.published_by = author.id;
     }
 
     return data;
+  });
+}
+
+/**
+ * @param {Array} posts
+ * @param {Array} users
+ * @returns {Array}
+ */
+export function exportPostsAuthors(posts, users) {
+  return posts.map((post) => {
+    return { post_id: post.id, user_id: post.author_id };
   });
 }
 
@@ -181,14 +362,148 @@ export async function exportImages() {
   fs.writeFileSync(`${__dirname}/.ghost/images.zip`, zipContent);
 }
 
-export async function initExport() {
+/**
+ * Check posts authors and if they are incorrect, fix them.
+ * @param {Array} posts
+ * @param {Array} users
+ * @returns {Promise<Boolean>}
+ */
+export async function checkPostsAuthorsAndFix(posts, users) {
+  const blogPosts = await findPosts(posts.length + 100);
+  const importedPosts = blogPosts.posts.filter((p) =>
+    posts.find((pl) => p.slug === pl.slug)
+  );
+
+  const invalidPosts = importedPosts.filter((p) => {
+    const post = posts.find((pl) => p.slug === pl.slug);
+    const user = users.find((u) => u.id === post.author_id);
+    return p.primary_author.email !== user.email;
+  });
+
+  if (!invalidPosts.length) {
+    return true;
+  }
+
+  const blogUsers = await findUsers();
+  const promises = invalidPosts.map((blogPost) => {
+    const post = posts.find((pl) => blogPost.slug === pl.slug);
+    const user = users.find((u) => u.id === post.author_id);
+
+    const blogUser = blogUsers.users.find((u) => u.slug === user.slug);
+
+    return requestGhostApi({
+      method: 'PUT',
+      url: `${GHOST_API}/posts/${blogPost.id}/?formats=mobiledoc%2Clexical`,
+      data: {
+        posts: [
+          {
+            ...blogPost,
+            primary_author: blogUser,
+            authors: [blogUser],
+          },
+        ],
+      },
+    });
+  });
+
+  await Promise.all(promises);
+  return true;
+}
+
+/**
+ * @param {Array<Object>} users
+ */
+export async function checkUsersRolesAndFix(users) {
+  const blogUsers = await findUsers();
+  const importedUsers = blogUsers.users.filter((u) =>
+    users.find((ul) => u.slug === ul.slug)
+  );
+  const invalidUsers = importedUsers.filter((u) => {
+    return (
+      !ADMIN_USERS.includes(u.name) &&
+      u.roles.find((r) => r.name !== 'Contributor')
+    );
+  });
+
+  if (!invalidUsers.length) {
+    return true;
+  }
+
+  const role = await findUserRoleByName('Contributor');
+  const promises = invalidUsers.map((user) => {
+    return requestGhostApi({
+      method: 'PUT',
+      url: `${GHOST_API}/users/${user.id}/?include=roles`,
+      data: {
+        users: [
+          {
+            ...user,
+            roles: [role],
+          },
+        ],
+      },
+    });
+  });
+
+  await Promise.all(promises);
+  return true;
+}
+
+/**
+ * Automatically upload any migration file to Ghost.
+ * It is similar to uploading manually through Dashboard -> Settings -> Labs -> Open importer
+ *
+ * @param {String} data path of the file you want to import
+ * @returns {Promise<Object>} result from axios
+ */
+export async function autoImportToGhost(dataPath) {
+  const cookie = await createGhostUserSession();
+
+  // Upload our local file
+  const form = new FormData();
+  form.append('importfile', fs.createReadStream(dataPath));
+
+  const resp = await axios.post(`${GHOST_API}/db`, form, {
+    headers: {
+      ...form.getHeaders(),
+      Referer: `${GHOST_SITE}/ghost/`,
+      Cookie: `${cookie}`,
+      Origin: GHOST_SITE,
+      'User-Agent':
+        'Mozilla/5.0 (Windows NT 10.0; rv:110.0) Gecko/20100101 Firefox/110.0',
+      Accept: 'text/plain, */*; q=0.01',
+      'Accept-Language': 'en-US,en;q=0.5',
+      'Accept-Encoding': 'gzip, deflate, br',
+      'X-Ghost-Version': '5.39',
+      'App-Pragma': 'no-cache',
+      'X-Requested-With': 'XMLHttpRequest',
+      DNT: '1',
+      Connection: 'keep-alive',
+      'Sec-Fetch-Dest': 'empty',
+      'Sec-Fetch-Mode': 'cors',
+      'Sec-Fetch-Site': 'same-origin',
+    },
+  });
+
+  return resp;
+}
+
+/**
+ * @param {boolean} [shouldAutoImport]
+ * @returns {Promise<Object>}
+ */
+export async function initExport(
+  shouldAutoImport = process.argv[2] === '--auto'
+) {
   // create a directory called .mobiledoc if it doesn't exist
   if (!fs.existsSync(`${__dirname}/.ghost`)) {
     fs.mkdirSync(`${__dirname}/.ghost`);
   }
 
   const users = exportUsers();
+  const rolesUsers = await exportRolesUsers(users);
   const posts = exportPosts(users);
+  const postsAuthors = exportPosts(posts, users);
   await exportImages();
 
   // create a database json in the format that Ghost expects
@@ -200,22 +515,46 @@ export async function initExport() {
     data: {
       posts,
       users,
+      posts_authors: postsAuthors,
+      roles_users: rolesUsers,
     },
   };
 
   // write to a file, and create that file if it doesn't exist
   // the file name is the same as the post's name, but ends in .json
-  fs.writeFileSync(
-    `${__dirname}/.ghost/migrationFromNext.json`,
-    JSON.stringify(db, null, 2),
-    { flag: 'w' }
-  );
+  const migrationFile = `${__dirname}/.ghost/migrationFromNext.json`;
+  fs.writeFileSync(migrationFile, JSON.stringify(db, null, 2), { flag: 'w' });
+
+  // Auto import using REST API
+  if (shouldAutoImport) {
+    console.log('importing blog posts to Ghost...');
+    await autoImportToGhost(migrationFile);
+
+    console.log('fixing migration errors...');
+    await checkPostsAuthorsAndFix(posts, users);
+    await checkUsersRolesAndFix(users);
+
+    console.log('importing images to Ghost...');
+    await autoImportToGhost(`${__dirname}/.ghost/images.zip`);
+  }
+
+  return db;
 }
 
 (async () => {
+  // Don't initialize import when running tests
+  if (process.env.NODE_ENV === 'test') {
+    return;
+  }
+
   console.log('initializing export...');
   await initExport();
-  console.log('export completed');
-  console.log('Open Ghost Dashboard -> Settings -> Labs');
-  console.log('Import .ghost/images.zip and migrationFromNext.json');
+
+  if (process.argv[2] !== '--auto') {
+    console.log('export completed');
+    console.log('Open Ghost Dashboard -> Settings -> Labs');
+    console.log('Import .ghost/images.zip and migrationFromNext.json');
+  } else {
+    console.log(`successfully imported your data to ${GHOST_SITE}`);
+  }
 })();
--- convertToMobiledoc.spec.js
+++ convertToMobiledoc.spec.js
@@ -1,7 +1,45 @@
 import fs from 'fs';
 import * as Convert from './convertToMobiledoc.mjs';
+import { execSync } from 'child_process';
+import axios from 'axios';
+
+jest.setTimeout(1000 * 40);
+
+export async function refreshGhost() {
+  execSync('ghost stop -d ./ghost');
+  execSync('rm -rf ghost/content/data/ghost-local.db');
+  execSync('ghost start -d ./ghost');
+  axios({
+    method: 'POST',
+    url: `${Convert.GHOST_API}/authentication/setup/`,
+    data: {
+      setup: [
+        {
+          email: process.env.GHOST_USER,
+          name: process.env.GHOST_USER,
+          password: process.env.GHOST_PASSWORD,
+          blogTitle: 'Test Blog',
+        },
+      ],
+    },
+  });
+  return new Promise((resolve) => setTimeout(resolve, 1000 * 2));
+}
+
+// it('refresh ghost', async () => {
+//   await refreshGhost();
+// });
 
 describe('convertToMobiledoc', () => {
+  describe('createGhostUserSession()', () => {
+    it('should return user session key', async () => {
+      const result = await Convert.createGhostUserSession();
+      expect(typeof result).toEqual('string');
+      expect(result.length > 0).toEqual(true);
+      // console.log('result', result);
+    });
+  });
+
   describe('markdownToJson()', () => {
     it('should return an object with data parsed from markdown', () => {
       const markdown = fs.readFileSync(
@@ -40,6 +78,25 @@ describe('convertToMobiledoc', () => {
     });
   });
 
+  describe('exportRolesUsers()', () => {
+    it('should assign contributor role to all users except for Mike and Phil', async () => {
+      const users = Convert.exportUsers();
+      const result = await Convert.exportRolesUsers(users, '');
+
+      expect(Array.isArray(result)).toEqual(true);
+      // expect(result.length).toEqual(users.length - 2);
+
+      const resultKeys = result
+        .flatMap((r) => Object.keys(r))
+        .filter((value, index, arr) => arr.indexOf(value) === index);
+
+      expect(resultKeys).toEqual(['user_id', 'role_id']);
+      // expect(
+      //   result.filter((r) => r.role_id !== Convert.CONTRIBUTOR_ROLE_ID).length
+      // ).toEqual(0);
+    });
+  });
+
   describe('exportPosts()', () => {
     it('should return an array with posts information', async () => {
       const users = Convert.exportUsers();
@@ -51,12 +108,112 @@ describe('convertToMobiledoc', () => {
     });
   });
 
+  describe('exportPostsAuthors', () => {
+    it('should export the posts_authors relation', async () => {
+      const users = Convert.exportUsers();
+      const posts = Convert.exportPosts(users);
+      const result = Convert.exportPostsAuthors(posts, users);
+      expect(Array.isArray(result)).toEqual(true);
+
+      const resultKeys = result
+        .flatMap((r) => Object.keys(r))
+        .filter((value, index, arr) => arr.indexOf(value) === index);
+
+      expect(resultKeys).toEqual(['post_id', 'user_id']);
+    });
+  });
+
   describe('exportImages()', () => {
     it('should export all the images', async () => {
       await Convert.exportImages();
       expect(fs.lstatSync(`${__dirname}/.ghost/images.zip`).isFile()).toEqual(
         true
       );
+    });
+  });
+
+  describe('checkPostsAuthorsAndFix()', () => {
+    it('should correctly assign authors', async () => {
+      // refreshGhost();
+      const users = Convert.exportUsers();
+      const posts = Convert.exportPosts(users);
+
+      let result;
+      try {
+        result = await Convert.checkPostsAuthorsAndFix(posts, users);
+      } catch (error) {
+        console.log('error:', error);
+        expect(error).toEqual(undefined);
+      }
+      expect(result).toEqual(true);
+
+      const blogPosts = await Convert.findPosts(posts.length + 100);
+      const importedPosts = blogPosts.posts.filter((p) =>
+        posts.find((pl) => p.slug === pl.slug)
+      );
+      const invalidPosts = importedPosts.filter((p) => {
+        const post = posts.find((pl) => p.slug === pl.slug);
+        const user = users.find((u) => u.id === post.author_id);
+        return p.primary_author.email !== user.email;
+      });
+      expect(invalidPosts.length).toEqual(0);
+    });
+  });
+
+  describe('checkUsersRolesAndFix()', () => {
+    it('should correctly assign authors', async () => {
+      // refreshGhost();
+      const users = Convert.exportUsers();
+
+      let result;
+      try {
+        result = await Convert.checkUsersRolesAndFix(users);
+      } catch (error) {
+        console.log('error:', error);
+        expect(error).toEqual(undefined);
+      }
+      expect(result).toEqual(true);
+
+      const blogUsers = await Convert.findUsers();
+      const importedUsers = blogUsers.users.filter((u) =>
+        users.find((ul) => u.slug === ul.slug)
+      );
+      const invalidUsers = importedUsers.filter((u) => {
+        return (
+          !Convert.ADMIN_USERS.includes(u.name) &&
+          u.roles.find((r) => r.name !== 'Contributor')
+        );
+      });
+      expect(invalidUsers.length).toEqual(0);
+    });
+  });
+
+  describe('autoImportToGhost()', () => {
+    it('should automatically upload the migration file', async () => {
+      // refreshGhost();
+      const db = await Convert.initExport(false);
+
+      let result;
+      try {
+        result = await Convert.autoImportToGhost(
+          `${__dirname}/.ghost/migrationFromNext.json`
+        );
+      } catch (error) {
+        console.log('error:', error);
+        expect(error).toEqual(undefined);
+      }
+
+      expect(result.status).toEqual(200);
+      expect(result.data.problems.length).toEqual(0);
+
+      const data = await Convert.findPosts(db.data.posts.length);
+      expect(data.posts.length).toEqual(db.data.posts.length);
+
+      const nonAdminPosts = data.posts.filter((p) =>
+        p.authors.find((a) => a.id !== '1')
+      );
+      // console.log('nonAdminPosts:', nonAdminPosts);
+      expect(nonAdminPosts.length).not.toEqual(0);
     });
   });
 });
